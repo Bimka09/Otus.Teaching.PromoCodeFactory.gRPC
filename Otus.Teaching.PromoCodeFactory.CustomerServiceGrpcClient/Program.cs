55a484f4b58ae2cae7dd70990c34ee0ea456823d
Bimka09 <47120311+Bimka09@users.noreply.github.com>
2024-08-22 21:35:10 +0300

    Add project files.

---
+++ Otus.Teaching.PromoCodeFactory.CustomerServiceGrpcClient/Program.cs
@@ -0,0 +1,91 @@
+﻿using Grpc.Net.Client;
+using Otus.Teaching.PromoCodeFactory.CustomerServiceGrpcClient.Protos.CustomersService;
+using static Otus.Teaching.PromoCodeFactory.CustomerServiceGrpcClient.Protos.CustomersService.CustomersService;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new CustomersServiceClient(channel);
+var isStopped = false;
+var createdCutomerGuid = Guid.Empty;
+while(!isStopped)
+{
+    Console.WriteLine($"Введите номер процедуры: {Environment.NewLine}" +
+        $"1 - Создать пользователя {Environment.NewLine} " +
+        $"2 - Получить созданного пользователя по guid {Environment.NewLine} " +
+        $"3 - Получить всех пользователей{Environment.NewLine} " +
+        $"4 - Обновить созданного пользователя{Environment.NewLine} " +
+        $"5 - Удалить созданного пользователя{Environment.NewLine} " +
+        $"6 - Завершить работу{Environment.NewLine}");
+    var id = Convert.ToInt32(Console.ReadLine());
+    switch (id)
+    {
+        case 1:
+            createdCutomerGuid = await CreateNewCustomerAsync(client);
+            break;
+
+        case 2:
+            await GetCustomerAsync(client, createdCutomerGuid);
+            break;
+
+        case 3:
+            await GetCustomersAsync(client);
+            break;
+
+        case 4:
+            await UpdateCustomerAsync(client, createdCutomerGuid);
+            break;
+
+        case 5:
+            await DeleteCustomersAsync(client, createdCutomerGuid);
+            break;
+
+        case 6:
+            isStopped = true;
+            return;
+    }
+}
+Console.WriteLine("Завершено");
+
+async Task<Guid> CreateNewCustomerAsync(CustomersServiceClient client)
+{
+    var request = new CreateCustomerRequest()
+    {
+        FirstName = "gRPC",
+        LastName = "TestCreated",
+        Email = "TestCreated@gmail.com",
+    };
+    request.PreferenceIds.Add("76324c47-68d2-472d-abb8-33cfa8cc0c84");
+    var customerId = await client.CreateCustomerAsync(request);
+    Console.WriteLine($"Создан покупатель с идентификатором {customerId}");
+    return Guid.Parse(customerId.Id);
+}
+async Task GetCustomerAsync(CustomersServiceClient client, Guid createdCutomerGuid)
+{
+    var customer = await client.GetCustomerAsync(new Identifier() { Id = createdCutomerGuid.ToString()});
+    Console.WriteLine($"Получен покупатель {customer}");
+}
+
+async Task GetCustomersAsync(CustomersServiceClient client)
+{
+    var customers = await client.GetCustomersAsync(new Empty());
+    foreach (var customer in customers.Customers)
+        Console.WriteLine($"Получен покупатель {customer}");
+}
+
+async Task UpdateCustomerAsync(CustomersServiceClient client, Guid createdCutomerGuid)
+{
+    var request = new CreateCustomerRequest()
+    {
+        FirstName = "gRPC",
+        LastName = "TestUpdated",
+        Email = "TestUpdated@gmail.com",
+    };
+    var customers = await client.EditCustomersAsync(new EditCustomerRequest() { Id = createdCutomerGuid.ToString(), Customer = request });
+    var customer = await client.GetCustomerAsync(new Identifier() { Id = createdCutomerGuid.ToString() });
+    Console.WriteLine($"Покупатель обновлен {customer}");
+}
+
+async Task DeleteCustomersAsync(CustomersServiceClient client, Guid createdCutomerGuid)
+{
+    var customers = await client.DeleteCustomerAsync(new Identifier() { Id = createdCutomerGuid.ToString() });
+    Console.WriteLine($"Покупатель удален");
+}
---
+++ Otus.Teaching.PromoCodeFactory.WebHost/Services/CustomersGrpsService.cs
@@ -0,0 +1,112 @@
+﻿using Grpc.Core;
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using Otus.Teaching.PromoCodeFactory.WebHost.Mappers;
+using Otus.Teaching.PromoCodeFactory.WebHost.Protos.CustomersService;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Otus.Teaching.PromoCodeFactory.WebHost.Services
+{
+    public class CustomersGrpsService(IRepository<Customer> customerRepository, IRepository<Preference> preferenceRepository) : CustomersService.CustomersServiceBase
+    {
+        private readonly IRepository<Customer> _customerRepository = customerRepository;
+        private readonly IRepository<Preference> _preferenceRepository = preferenceRepository;
+
+        public override async Task<Identifier> CreateCustomer(CreateCustomerRequest request, ServerCallContext context)
+        {
+            var preferences = await _preferenceRepository.GetRangeByIdsAsync(request.PreferenceIds.Select(x => Guid.Parse(x)).ToList());
+
+            var customer = new Customer()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Preferences = preferences.Select(x => new CustomerPreference()
+                {
+                    Preference = x,
+                    PreferenceId = x.Id
+                }).ToList()
+            };
+
+            await _customerRepository.AddAsync(customer);
+
+            return new Identifier() { Id = customer.Id.ToString()};
+
+        }
+
+        public override async Task<CustomerShortResponse> GetCustomer(Identifier request, ServerCallContext context)
+        {
+            var customer = await _customerRepository.GetByIdAsync(Guid.Parse(request.Id));
+
+            var response = new CustomerShortResponse()
+            {
+                Id = customer.Id.ToString(),
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Email = customer.Email,
+            };
+
+            return response;
+        }
+
+        public override async Task<CustomerShortResponseList> GetCustomers(Empty request, ServerCallContext context)
+        {
+            var customers = await _customerRepository.GetAllAsync();
+            var response = new CustomerShortResponseList();
+
+            foreach (var customer in customers)
+            {
+                response.Customers.Add(new CustomerShortResponse()
+                {
+                    Id = customer.Id.ToString(),
+                    Email = customer.Email,
+                    FirstName = customer.FirstName,
+                    LastName = customer.LastName
+                });
+            }
+
+            return response;
+        }
+
+
+        public override async Task<Empty> EditCustomers(EditCustomerRequest request, ServerCallContext context)
+        {
+            var customer = await _customerRepository.GetByIdAsync(Guid.Parse(request.Id));
+
+            if (customer == null) return new Empty();
+
+            var preferences = await _preferenceRepository.GetRangeByIdsAsync(request.Customer.PreferenceIds.Select(x => Guid.Parse(x)).ToList());
+
+            await _customerRepository.UpdateAsync(customer);
+
+            customer.Preferences = preferences.Select(x => new CustomerPreference()
+            {
+                CustomerId = customer.Id,
+                Preference = x,
+                PreferenceId = x.Id
+            }).ToList();
+
+            customer.Email = request.Customer.Email;
+            customer.FirstName = request.Customer.FirstName;
+            customer.LastName = request.Customer.LastName;
+
+            await customerRepository.UpdateAsync(customer);
+
+            return new Empty();
+        }
+
+        public override async Task<Empty> DeleteCustomer(Identifier request, ServerCallContext context)
+        {
+            var customer = await _customerRepository.GetByIdAsync(Guid.Parse(request.Id));
+
+            if (customer == null) return new Empty();
+
+            await _customerRepository.DeleteAsync(customer);
+
+            return new Empty();
+        }
+    }
+}
